41545a87c63c9aded5922c6e738b311153977606
“Kevin_Gamboa_Montaño” <kevincitocapo23102011@gmail.comgit config --global user.email kevincitocapo23102011@gmail.com>
2024-02-26 23:57:48 -0400

    Implementamos que cualquier anio multiplo de 100 pero no de 400 no es bisiesto - prueba 1 (1700)

--- src/bisiesto.js
+++ src/bisiesto.js
@@ -1,8 +1,8 @@
 function get_bisiesto(n) {
  if(n % 400 === 0){
      return "SI, es bisiesto";
- }else{
-     return "No, es bisiesto"
+ }else if(n % 100 === 0 && n % 400 != 0){
+     return "No es bisiesto"
  }
 }
   
